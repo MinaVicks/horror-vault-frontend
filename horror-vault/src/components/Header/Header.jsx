7aa04d507b745987f751d9ce349a6753c2d9847a
Magaly Romero <vico.m.romero@gmail.com>
2026-07-15 12:47:00 -0600

    Navigation prueba

--- horror-vault/src/components/Header/Header.jsx
+++ horror-vault/src/components/Header/Header.jsx
@@ -1,5 +1,14 @@
+import "./Header.css";
+import Navigation from "../Navigation/Navigation.jsx";
+import Logo from "../../assets/horror vault_logofull.svg";
+
 function Header() {
-  return  <div><h1>Horrur Vault</h1></div> ;
+  return  <div className="header__container">
+    <Navigation />
+    <div className="header__logo">
+      <img src={Logo} alt="Logo Horror Vault" className="header__logo" />
+    </div>
+    </div> ;
 }
 
 export default Header;
--- horror-vault/src/components/Navigation/Navigation.jsx
+++ horror-vault/src/components/Navigation/Navigation.jsx
@@ -1,5 +1,16 @@
+import "./Navigation.css";
+import logoicon from "../../assets/horror vault-icon.svg";
+
 function Navigation() {
-  return <div>Navigation</div>;
+  return  <div className="navigation__container">
+        <div className="navigation__logo">
+          <img src={logoicon} alt="Logo Horror Vault" className="navigation__logo" />
+        </div> 
+        <div className="navigation__buttons">
+         <div className="navigation__home">Home</div>
+         <div className="navigation__movies">Peliculas</div>
+        </div>
+        </div>;
 }
 
 export default Navigation;
