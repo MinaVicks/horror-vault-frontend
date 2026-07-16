e49af3e4563301e1f12b651f0fd182915557a02c
Magaly Romero <vico.m.romero@gmail.com>
2026-07-15 18:33:51 -0600

    Navigation fix

--- horror-vault/src/components/Navigation/Navigation.jsx
+++ horror-vault/src/components/Navigation/Navigation.jsx
@@ -6,20 +6,13 @@ import { Link } from "react-router-dom";
 function Navigation() {
   return  <div className="navigation__container">
             <nav className="navbar">
-                <div className="navigation__logo">
-                      <img src={logoicon} alt="Logo Horror Vault" className="navigation__logo" />
-                </div> 
-                
-                <ul className="nav-links">
-                      <li >
-                        <Link to="/" className="navigation__home">Home</Link>
-                      </li>
-
-                      <li >
-                        <Link to="/movies" className="navigation__movies">Peliculas</Link>
-                      </li>
-                </ul>
-              </nav>
+               <div className="navigation__logo"> 
+                <img src={logoicon} alt="Logo Horror Vault" className="navigation__logo" /> 
+                </div>
+                 <ul className="nav-links"> 
+                  <li > <Link to="/" className="navigation__home">Home</Link> </li> 
+                  <li > <Link to="/movies" className="navigation__movies">Peliculas</Link> </li> </ul>
+                   </nav>
             </div>;
 }
 
--- horror-vault/src/components/SearchForm/SearchForm.jsx
+++ horror-vault/src/components/SearchForm/SearchForm.jsx
@@ -1,3 +1,5 @@
+import "./SearchForm.css";
+
 function SearchForm({
   query,
   setQuery,
@@ -9,19 +11,24 @@ function SearchForm({
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <div >
+    <form onSubmit={handleSubmit} className="form__search-container">
       <input
         type="text"
-        placeholder="Search horror movies..."
+        placeholder="Encuentra tu próxima pelicula..."
+        className="form__search-input"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         required
       />
 
-      <button type="submit">
-        Search
+      <button type="submit" className="form__search-button">
+        Buscar
       </button>
+
+      <p className="form__search-instruction">Busca la película que desees utilizando el título, el género, o incluso una palabra que esté incluida en el título; con eso será suficiente.</p>
     </form>
+    </div>
   );
 }
 
